ed16064b73856571975d2ebdba09722723b76655
Caryssa Perez <caryssaperez@gmail.com>
2018-12-27 17:43:53 -0500

    Start chapter 4

---
+++ src/chapter4/ownership.rs
@@ -0,0 +1,9 @@
+// Just gonna rip out a paragraph from the book:
+//
+// "Some languages have garbage collection that constantly looks for no longer used
+// memory as the program runs; in other languages, the programmer must explicitly 
+// allocate and free the memory. Rust uses a third approach: memory is managed 
+// through a system of ownership with a set of rules that the compiler checks at 
+// compile time."
+//
+// The first approach is like JS, the second like C.
