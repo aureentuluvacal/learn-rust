b379e325a62ed20645242adec0fea792aade2b7f
Caryssa Perez <caryssaperez@gmail.com>
2018-12-27 17:40:57 -0500

    Finish chapter 3

--- src/chapter3/functions.rs
+++ src/chapter3/functions.rs
@@ -1,11 +1,23 @@
 fn example_function() {
     println!("Bacon");
 
-    another_function();
+    another_function(37);
+    println!("{} more crab rangoons!", yet_another_function());
 }
 
-fn another_function() {
-    println!("Crab rangoons");
+// Function with a typed argument.
+fn another_function(x: i32) {
+    let y = {
+        let z = 4; // statement, doesn't return a value, ends in semicolon
+        x + z + 1 // expression, returns a value
+    };
+
+    println!("{} crab rangoons", y);
+}
+
+// Can specify the type with an arrow after the arguments.
+fn yet_another_function() -> i32 {
+    5 // Just like Ruby there's an implied return in a function like this.
 }
 
 fn main() {
