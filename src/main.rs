c687c81decad8a7d2840773b84764be92c0ff1c1
Caryssa Perez <caryssaperez@gmail.com>
2019-01-02 14:05:54 -0500

    This too

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,11 @@
-#[derive(Debug)]
-// Different discrete types of a thing. 
-enum Plan {
-    Enterprise,
-    Pro,
-    Plus,
-    Standard
-}
-
-// Can define structs in enums.
-enum Coordinates {
-    Cartesian { x: i32, y: i32, z: i32 },
-    Spherical { pi: f32, phi: f32, r: i32 }
-}
-
-// Can add methods to enums just like structs.
-impl Plan {
-    fn generate_bill(&self) -> u32 {
-        match self {
-            Plan::Enterprise => 1000,
-            Plan::Pro => 750,
-            Plan::Plus => 500,
-            Plan::Standard => 250
-        }
-    }
-}
-
-struct Company {
-    plan: Plan,
-    name: String
-}
-
+// If we care about only one case then we use if let.
 fn main() {
-    let coordinates = Coordinates::Cartesian {
-        x: 3, 
-        y: -1,
-        z: 0
-    };
-    let company = Company {
-        plan: Plan::Pro,
-        name: String::from("Crab Rangoon")
-    };
-
-    println!("The {:?} plan", Plan::Enterprise);
-    println!("Company {} is on the {:?} plan and owes ${}", company.name, company.plan, company.plan.generate_bill());
+    let some_u8_value = Some(3);
+    if let Some(3) = some_u8_value {
+        println!("Three");
+    } else {
+        // Can also include an else block which functions the same as
+        // a "_" in a match.
+        println!("Not three!");
+    }
 }
