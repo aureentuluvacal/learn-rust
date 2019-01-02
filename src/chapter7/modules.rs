83c522c350c40fdea2011b120c5562a8729dc214
Caryssa Perez <caryssaperez@gmail.com>
2019-01-02 14:56:37 -0500

    Just note taking for chapter 7

---
+++ src/chapter7/modules.rs
@@ -0,0 +1,7 @@
+// Modules can be nested like files in directories.
+// By default, modules, their functions and its contents
+// are private. We need to add the keyword "pub" to make it accessible.
+
+// I can access parent module contents but not child module contents.
+// Adding "pub" to structs makes the struct public but not its fields.
+// Adding "pub" to enums makes its variants public as well.
