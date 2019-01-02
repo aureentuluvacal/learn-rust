5c4968dd3a53b01dcb2451dd348a9ae9aed3305e
Caryssa Perez <caryssaperez@gmail.com>
2019-01-02 08:49:38 -0500

    More enum stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #[derive(Debug)]
+// Different discrete types of a thing. 
 enum Plan {
     Enterprise,
     Pro,
@@ -6,18 +7,40 @@ enum Plan {
     Standard
 }
 
-fn generate_bill(plan : Plan) -> u32 {
-    match plan {
-        Plan::Enterprise => 1000,
-        Plan::Pro => 750,
-        Plan::Plus => 500,
-        Plan::Standard => 250
+// Can define structs in enums.
+enum Coordinates {
+    Cartesian { x: i32, y: i32, z: i32 },
+    Spherical { pi: f32, phi: f32, r: i32 }
+}
+
+// Can add methods to enums just like structs.
+impl Plan {
+    fn generate_bill(&self) -> u32 {
+        match self {
+            Plan::Enterprise => 1000,
+            Plan::Pro => 750,
+            Plan::Plus => 500,
+            Plan::Standard => 250
+        }
     }
 }
 
+struct Company {
+    plan: Plan,
+    name: String
+}
+
 fn main() {
-    let plan = Plan::Pro;
+    let coordinates = Coordinates::Cartesian {
+        x: 3, 
+        y: -1,
+        z: 0
+    };
+    let company = Company {
+        plan: Plan::Pro,
+        name: String::from("Crab Rangoon")
+    };
 
     println!("The {:?} plan", Plan::Enterprise);
-    println!("The {:?} plan is ${}", Plan::Pro, generate_bill(plan));
+    println!("Company {} is on the {:?} plan and owes ${}", company.name, company.plan, company.plan.generate_bill());
 }
