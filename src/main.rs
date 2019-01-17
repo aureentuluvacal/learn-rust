9b719e42e1e717804e2dc01f2c7e34295bc8823c
Caryssa Perez <caryssaperez@gmail.com>
2019-01-16 21:21:58 -0500

    Start chapter 10

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,64 @@
-// We can use the Result enum to handle recoverable errors, like trying to write
-// to a file that doesn't exist.
-
-use std::fs::File;
-use std::io;
-use std::io::ErrorKind;
-use std::io::Read;
-
-// This way we manually handle errors and return early if there's an error reading
-// the file.
-fn read_username_from_file() -> Result<String, io::Error> {
-    let f = File::open("hello.txt");
-
-    let mut f = match f {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-
-    let mut s = String::new();
-
-    match f.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
-        Err(e) => Err(e),
-    }
+// Can define a struct to use any type. The type that we eventually
+// in the fields of the struct has to be the same type if only one
+// type parameter is passed.
+struct Course<T, U> {
+    name: T,
+    subject: T,
+    credits: U,
 }
 
-// This way uses ? which short circuits the function if any errors occur,
-// implicitly returning early if an error is encountered. Can only be used with
-// functions that return a Result.
-fn read_username_from_file_with_question_mark() -> Result<String, io::Error> {
-    // fs::read_to_string("hello.txt") <-- can be boiled down to just that at the end
-    // of the day.
-    let mut f = File::open("hello.txt")?;
-    let mut s = String::new();
-    f.read_to_string(&mut s)?;
-    Ok(s)
+// Can do the same with enums.
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+// The following two functions differ only in parameter type
+// and return type.
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn generic_largest<T>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
 }
 
 fn main() {
-    let f = File::open("hello.txt");
+    let number_list = vec![34, 50, 25, 100, 65];
 
-    // Handle the error with a match, which isn't as sexy as using closures and unwrap_or_else.
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!(
-                    "Tried to create file but there was a problem: {:?}",
-                    e
-                ),
-            },
-            other_error => panic!(
-                "There was a problem opening the file: {:?}",
-                other_error
-            ),
-        },
-    };
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
 
-    // Or handle by propogating error to what is calling the code.
-    let result = read_username_from_file();
-    print!("{:?}", result);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
 }
