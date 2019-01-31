83632d8cfb2149be7f7d4c9ff5b6b35ed1a5d503
Caryssa Perez <caryssaperez@gmail.com>
2019-01-30 21:12:24 -0500

    Move code

--- src/lib.rs
+++ src/lib.rs
@@ -98,3 +98,25 @@ Trust me.";
         );
     }
 }
+
+// And the content's of main.rs:
+
+// use learn_rust;
+// use learn_rust::Config;
+// use std::env;
+// use std::process;
+
+// fn main() {
+//     let args: Vec<String> = env::args().collect();
+
+//     let config = Config::new(&args).unwrap_or_else(|err| {
+//         eprintln!("Problem parsing arguments: {}", err);
+//         process::exit(1);
+//     });
+
+//     if let Err(e) = learn_rust::run(config) {
+//         eprintln!("Application error: {}", e);
+
+//         process::exit(1);
+//     }
+// }
