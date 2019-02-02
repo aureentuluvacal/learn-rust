f3db34dc8eef6e4ad81129179e8d3e93914e2c98
Caryssa Perez <caryssaperez@gmail.com>
2019-02-02 10:49:30 -0500

    Start closures

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,73 @@
-use learn_rust;
-use learn_rust::Config;
-use std::env;
-use std::process;
+use std::collections::HashMap;
+use std::thread;
+use std::time::Duration;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    values: HashMap<u32, u32>,
+}
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            values: HashMap::new(),
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.values.get(&arg) {
+            Some(&v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.values.insert(arg, v);
+                v
+            }
+        }
+    }
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    // Closures have an implied type. That type must remain the same
+    // whenever the closure is used.
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
     });
 
-    if let Err(e) = learn_rust::run(config) {
-        eprintln!("Application error: {}", e);
-
-        process::exit(1);
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps!", expensive_result.value(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
+        }
     }
+}
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+
+    // Closures can use variables that are defined in the same scope as it.
+    // Can't do that with functions.
+    let x = 4;
+    let equal_to_x = |z| z == x;
+    let y = 4;
+
+    assert!(equal_to_x(y));
 }
