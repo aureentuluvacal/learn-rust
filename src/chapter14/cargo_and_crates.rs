89b518dacb4b6f97d53fff3b8569b8a8cb347e86
Caryssa Perez <caryssaperez@gmail.com>
2019-02-03 12:34:36 -0500

    Take notes from Chapter 14

---
+++ src/chapter14/cargo_and_crates.rs
@@ -0,0 +1,10 @@
+// Can setup specific configs for dev and prod in the Cargo.toml.
+
+// Can use documentation comments (///) that generates HTML for
+// how to use your crate. It uses markdown. Running `cargo doc --open`
+// will generate the HTML and open it in the browser.
+
+// Can use //! to document the crate root in lib.rs.
+
+// Can separate work into separate crates to simplify projects before they get
+// too big using workspaces. Set up the config in the Cargo.toml.
